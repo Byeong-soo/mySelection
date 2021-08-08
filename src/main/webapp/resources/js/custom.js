ce7c4cab2394856455e637c0f52a7c7fc8a1e867
byeongsoo <ubs4939@naver.com>
2021-08-09 03:36:05 +0900

    스크롤바 스크립트로 조절 modal open 시 블러처리 회원가입 페이지 상세정보 적기 토글 구현

---
+++ src/main/webapp/resources/js/custom.js
@@ -0,0 +1,64 @@
+$('#navLoginBtn').click(function () {
+    $('#loginModal').modal('open');
+    $('div[name=supreme-container]').addClass('blur');
+
+});
+
+function scrollbarDisappear() {
+    $("body::-webkit-scrollbar").css({
+        "display" : "none"
+    });
+    
+};
+
+function scrollbarAppear() {
+    $("body::-webkit-scrollbar").css({
+        "display" : "auto"
+    });
+
+};
+
+
+
+// $("body").on("mousewheel", function (event) {
+//     let wheel = event.originalEvent.wheelDelta;
+//     console.log(wheel);
+//     if(wheel>0){
+//         scrollbarDisappear();
+//         console.log("올라간다");
+//     } else {
+//         scrollbarAppear();
+//         console.log("내려간다");
+//     }
+// });
+
+$(window).scroll(function() {
+    $('body').addClass('scrolling');
+    clearTimeout($.data(this, 'scrollTimer'));
+    $.data(this, 'scrollTimer', setTimeout(function() {
+        $('body').removeClass('scrolling');
+    }, 250));
+});
+
+
+function addtionalToggle(){
+    if($('#additional-info').css('display') == 'none'){
+        $('#additional-info').show();
+    }else{
+        $('#additional-info').hide();
+    }
+}
+
+
+    let logout = $("#logout");
+    logout.click(function () {
+    console.log("di")
+});
+    // $('#logout').click(function () {
+    //     console.log("di")
+    //     setTimeout(function () {
+    //         M.toast({html: '로그아웃 되었습니다.', classes: 'rounded indigo darken-3 white-text'});
+    //     },1000)
+    // })
+
+
