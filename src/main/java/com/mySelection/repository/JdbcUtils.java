5c588ac4b29b26c1357ccc9658ec385fb3e03309
byeongsoo <ubs4939@naver.com>
2021-08-09 03:28:11 +0900

    포트번호 변경

--- src/main/java/com/mySelection/repository/JdbcUtils.java
+++ src/main/java/com/mySelection/repository/JdbcUtils.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class JdbcUtils {
     // MySQL DB접속정보
-    public static final String url = "jdbc:mysql://localhost:3307/jspdb?useUnicode=true&characterEncoding=utf8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=Asia/Seoul";
+    public static final String url = "jdbc:mysql://localhost:3306/myselectiondb?useUnicode=true&characterEncoding=utf8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=Asia/Seoul";
     public static final String user = "myselection";
     public static final String passwd = "@DBqudtn587";
 
