cfdfe44c5f537e06a60c4db4de83f65b39f8c492
byeongsoo <ubs4939@naver.com>
2021-08-10 03:16:26 +0900

    사용자가 입력한 VODB

---
+++ src/main/java/com/mySelection/domain/ClassificationVO.java
@@ -0,0 +1,13 @@
+package com.mySelection.domain;
+
+import lombok.Data;
+
+@Data
+public class ClassificationVO {
+    String title;
+    String type;
+    int index;
+    String first;
+    String second;
+    String third;
+}
