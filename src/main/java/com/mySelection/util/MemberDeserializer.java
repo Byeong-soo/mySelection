c72328ee27d85058404e8094cc321ca1748d4702
byeongSoo <ubs4939@naver.com>
2021-09-15 18:23:25 +0900

    member JsonDeserializer 커스텀

---
+++ src/main/java/com/mySelection/util/MemberDeserializer.java
@@ -0,0 +1,42 @@
+package com.mySelection.util;
+
+import com.google.gson.*;
+import com.mySelection.domain.MemberVO;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.lang.reflect.Type;
+import java.sql.Timestamp;
+
+public class MemberDeserializer implements JsonDeserializer<MemberVO> {
+
+    @Override
+    public MemberVO deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+
+        MemberVO memberVO = null;
+
+
+        if (json.isJsonObject()) {
+            JsonObject jsonObject = json.getAsJsonObject(); // {}
+
+            memberVO = new MemberVO();
+            memberVO.setId(jsonObject.get("id").getAsString());
+            memberVO.setPasswd(jsonObject.get("passwd").getAsString());
+            memberVO.setEmail(jsonObject.get("email").getAsString());
+            memberVO.setNickname(jsonObject.get("nickname").getAsString());
+            memberVO.setProfileImage(jsonObject.get("profileImage").getAsString());
+            memberVO.setBirthday(jsonObject.get("birthday").getAsString());
+            memberVO.setGender(jsonObject.get("gender").getAsString());
+            memberVO.setRecvEmail(jsonObject.get("receiveEmail").getAsString());
+            memberVO.setRegDate(new Timestamp(System.currentTimeMillis()));
+            memberVO.setJoinType("J");
+
+
+            // 비밀번호 암호화 수정하기
+            String hashedPw = BCrypt.hashpw(memberVO.getPasswd(), BCrypt.gensalt());
+            memberVO.setPasswd(hashedPw);
+
+        }
+        return memberVO;
+
+    }
+}
